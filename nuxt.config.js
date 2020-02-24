dc5542fde7b7bd5bf7f3c716d76e64c8ea5d825d
Mohamed Amine Fredj <mfr@ip-label.com>
2020-02-24 15:20:55 +0100

    starter verstion

---
+++ nuxt.config.js
@@ -0,0 +1,62 @@
+module.exports = {
+  mode: 'universal',
+  head: {
+    titleTemplate: 'Amine',
+    meta: [
+      { property: 'og:image', content: 'https://user-images.githubusercontent.com/904724/58238637-f189ca00-7d47-11e9-8213-ae072d7cd3aa.png' },
+      { property: 'twitter:card', content: 'summary_large_image' },
+      { property: 'twitter:site', content: '@nuxt_js' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'dns-prefetch', href: 'https://api.hackerwebapp.com' },
+      { rel: 'preconnect', href: 'https://api.hackerwebapp.com' }
+    ]
+  },
+
+  loading: {
+    color: '#00C48D'
+  },
+
+  manifest: {
+    name: 'Amine',
+    short_name: 'Amine',
+    description: 'Amine',
+    theme_color: '#2F495E',
+    start_url: ''
+  },
+
+  buildModules: [
+    '@nuxtjs/pwa',
+    '@nuxtjs/vuetify',
+    '@nuxtjs/dotenv',
+    '@nuxtjs/axios'
+  ],
+
+  vuetify: {
+    /* module options */
+    treeShake: true,
+    optionsPath: "./vuetify.options.js"
+  },
+
+  axios: {
+    baseURL: '/'
+  },
+
+  plugins: [
+  ],
+
+  render: {
+    http2: {
+      push: true
+    },
+    static: {
+      maxAge: '1y',
+      setHeaders (res, path) {
+        if (path.includes('sw.js')) {
+          res.setHeader('Cache-Control', `public, max-age=${15 * 60}`)
+        }
+      }
+    }
+  }
+}
---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
+const app = express()
+
+// Import and Set Nuxt.js options
+const config = require('../nuxt.config.js')
+config.dev = process.env.NODE_ENV !== 'production'
+
+async function start () {
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config)
+
+  const { host, port } = nuxt.options.server
+
+  await nuxt.ready()
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  }
+
+  app.get('/api/test', function(req, res) {
+    res.send('hello world');
+  });
+
+  // Give nuxt middleware to express
+  app.use(nuxt.render)
+
+  // Listen the server
+  app.listen(9090, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${9090}`,
+    badge: true
+  })
+}
+start()
