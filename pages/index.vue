af79b247483fe0bf92d3bdb8c75dbafa185fc3cf
Mohamed Amine Fredj <mfr@ip-label.com>
2020-02-26 14:30:05 +0100

    test hyd

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
   <div> 
-    <LazyHydrate when-visible>
-      <router-link :to="{ path: 'ad-admin/overview' }">backOffice</router-link>
+    <router-link :to="{ path: 'ad-admin/overview' }">backOffice</router-link>
+    <v-lazy when-visible>
       <h1>this is the back base container</h1>
-    </LazyHydrate>
+    </v-lazy>
   </div>
 </template>
 
 <script>
-import LazyHydrate from 'vue-lazy-hydration'
+import VLazy from 'vue-lazy-hydration'
 export default {
   components: {
-    LazyHydrate
+    VLazy
   }
 }
 </script>
