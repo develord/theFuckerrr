92b9b1dc0bfe2669da7bc8730500798651bc023f
Mohamed Amine Fredj <mfr@ip-label.com>
2020-02-26 14:15:34 +0100

    fix 2

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,17 @@
 <template>
   <div> 
+    <LazyHydrate when-visible>
       <router-link :to="{ path: 'ad-admin/overview' }">backOffice</router-link>
       <h1>this is the back base container</h1>
+    </LazyHydrate>
   </div>
 </template>
 
 <script>
+import LazyHydrate from 'vue-lazy-hydration'
 export default {
+  components: {
+    LazyHydrate
+  }
 }
 </script>
